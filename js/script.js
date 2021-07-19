ec246ae4aea698345b7cbfa2c5c0b6a358525d11
Rajnish <rajnish17100042@gmail.com>
2021-07-19 21:12:39 +0530

    implemented navbar

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ function showPage() {
     document.getElementById("myDiv").style.display = "block";
 }
 
+//change menu icon to cross symbol
+function changeMenuIcon(x) {
+    x.classList.toggle("change");
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+
+}
